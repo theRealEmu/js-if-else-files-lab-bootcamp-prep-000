c2323eb1bf620c3ac64b6799b15c711ef2c0b00d
Emanuel Harms <emanuel.harms@gmx.de>
2018-10-11 20:26:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 var name = "Susan";
 var height = "74";
 
-if (name === "Susan") {
-  console.log("Name is correct!");
-} else {
-   console.log("Name is not correct! Expected: Susan");
-}
-
 // Don't worry about this
 module.exports = { name, height
 }
